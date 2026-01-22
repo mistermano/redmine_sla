bda273cbad2ca77e4814fa81fb8a85e18d3e591a
Arthur <cavaleiro.knight@gmail.com>
2026-01-22 10:47:13 -0300

    Update sla_rendering_helper.rb

--- lib/redmine_sla/helpers/sla_rendering_helper.rb
+++ lib/redmine_sla/helpers/sla_rendering_helper.rb
@@ -22,6 +22,7 @@
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
 
 module RedmineSla
+  module Helpers
   # Rendering helpers for SLA display logic.
   # Placed under a dedicated submodule for clarity.
   module SlaRenderingHelper
